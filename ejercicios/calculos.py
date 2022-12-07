f663206ad45c28f9808e11abf311cfec01aaa886
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-12-07 18:01:00 +0100

    puesto el main

--- ejercicios/calculos.py
+++ ejercicios/calculos.py
@@ -1,7 +1,8 @@
 from operaciones import *
 
 a, b, c, d = (10, 5, 0, "Hola")
-print("{} + {} = {}".format(a, b, suma(a, b)))
-print("{} - {} = {}".format(b, d, resta(b, d)))
-print("{} * {} = {}".format(b, b, producto(b, b)))
-print("{} / {} = {}".format(a, c, division(a, c)))
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, suma(a, b)))
+    print("{} - {} = {}".format(b, d, resta(b, d)))
+    print("{} * {} = {}".format(b, b, producto(b, b)))
+    print("{} / {} = {}".format(a, c, division(a, c)))
