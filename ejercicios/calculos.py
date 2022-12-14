69fad3b06cd69a19689082f3271122e44704b8ca
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-12-14 17:26:23 +0100

    puesto todo el código en formato pep8

--- ejercicios/calculos.py
+++ ejercicios/calculos.py
@@ -2,6 +2,7 @@ from ejercicios.operaciones import *
 
 a, b, c, d = (10, 5, 0, "Hola")
 
+
 def mainej1():
     print("{} + {} = {}".format(a, b, suma(a, b)))
     print("{} - {} = {}".format(b, d, resta(b, d)))
--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -1,22 +1,25 @@
-def suma(a,b):
+def suma(a, b):
     try:
         return a+b
     except TypeError:
         print("Error: Tipo de dato no válido")
 
-def resta(a,b):
+
+def resta(a, b):
     try:
         return a-b
     except TypeError:
         print("Error: Tipo de dato no válido")
 
-def producto(a,b):
+
+def producto(a, b):
     try:
         return a*b
     except TypeError:
         print("Error: Tipo de dato no válido")
 
-def division(a,b):
+
+def division(a, b):
     try:
         return a/b
     except TypeError:
