a3432fbb1df835f9f9047e86973da24620e1fc99
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-12-14 17:03:39 +0100

    creada la clase personaje con el constructor

--- ejercicios/gestor.py
+++ ejercicios/gestor.py
@@ -1,3 +1,14 @@
 from io import open
 import pickle
 
+class Personaje:
+
+    def __init__(self, nombre, vida, ataque, defensa, alcance):
+        self.nombre = nombre
+        self.vida = vida
+        self.ataque = ataque
+        self.defensa = defensa
+        self.alcance = alcance
+
+    def __str__(self):
+        return "{} : {} vida, {} ataque, {} defensa, {} alcance".format(self.nombre, self.vida, self.ataque, self.defensa, self.alcance)
